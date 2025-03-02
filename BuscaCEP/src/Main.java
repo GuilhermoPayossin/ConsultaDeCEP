13cf434be7c2c14c2c8ef606e0719d6c2e426ff0
Guilhermo <guilhermo.payossin@gmail.com>
2025-03-01 23:40:21 -0300

    Add files via upload

---
+++ BuscaCEP/src/Main.java
@@ -0,0 +1,22 @@
+import static javax.swing.JOptionPane.*;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ConsultaCep consultaCep =  new ConsultaCep();
+
+        var cep = showInputDialog("Digite um CEP para a busca");
+
+        try {
+            Endereco endereco = consultaCep.buscaEndereco(cep);
+            GeradorDeArquivo gerador = new GeradorDeArquivo();
+            gerador.cirarJson(endereco);
+            showMessageDialog(null, "Arquivo contendo as informações gerado com sucesso!");
+        } catch (RuntimeException | IOException e) {
+            System.out.println(e.getMessage());
+            showMessageDialog(null, "Finalizando o Programa");
+        }
+    }
+}
